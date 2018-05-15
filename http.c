8610e6fb1fc6eb350637ad385be0431b2d789f38
lang.yanjun <lang.yanjun@tianyuantechnology.com>
2018-05-15 14:41:33 +0800

    https post OK!

--- http.c
+++ http.c
@@ -203,11 +203,74 @@ error:
     return NULL;
 }
 
+int http_ssl_write(HTTP_CONN_ST *conn, const char *text, int len)
+{
+    int ret = -1;
+
+    printf("text = [%s]\n", text);
+    if (!conn) {
+        return -1;
+    }
+
+    ret = SSL_write (conn->ssl_handle, text, len);
+
+    return ret;
+}
+
+char *http_ssl_read(HTTP_CONN_ST *conn)
+{
+    char *rc = NULL;
+    int received = 0, count = 0;
+    int buffer_size = 1024;
+    int free_size = 0;
+
+    if (!conn) {
+        return NULL;
+    }
+
+    rc = malloc (buffer_size);
+    while (1) {
+        if (buffer_size - count <= 0) {
+            buffer_size *= 2;
+            rc = realloc (rc, buffer_size);
+            free_size = buffer_size - count;
+            memset(rc + count, 0, free_size);
+        }
+        received = SSL_read(conn->ssl_handle, rc + count, buffer_size - count);
+        printf("[received = %d]\n", received);
+        if (received > 0) {
+            count += received;
+        }
+        if (received < buffer_size - count)
+            break;
+    }
+    printf("buf=[%s]\n", rc);
+
+    return rc;
+}
+
 int main(void)
 {
+    HTTP_CONN_ST *conn = NULL;
+
     http_ssl_global_init();
     
-    http_ssl_conn_creat();
+    const char *post_data = "POST /smarthome-api/v1.1/gateway/signup HTTP/1.1\r\n"
+            "cache-control: no-cache\r\n"
+            "Postman-Token: 3ecc6216-724b-4d71-bf17-4e13ae57618a\r\n"
+            "User-Agent: PostmanRuntime/7.1.1\r\n"
+            "Accept: */*\r\n"
+            "Host: apis.t2.5itianyuan.com\r\n"
+            "content-type: application/x-www-form-urlencoded\r\n"
+            "accept-encoding: gzip, deflate\r\n"
+            "content-length: 345\r\n"
+            "Connection: keep-alive\r\n"
+            "\r\n"
+    
+            "gatewaySn=201703091158&macAddress=20-17-03-09-11-58&vendorId=HUADI&productId=GZ6200&hardwareVersion=20160608&softwareVersion=20160608&moduleList=%5B%7B%22moduleSn%22%3A%22MODULE-ZIGBEE-0001%22%2C%22vendorCode%22%3A%22HUADI%22%2C%22productCode%22%3A%22M1-001%22%2C%22moduleType%22%3A%22zigbee%22%2C%22macAddress%22%3A%2211-22-33-44-55-66%22%7D%5D\r\n\r\n";
+    conn = http_ssl_conn_creat();
+    http_ssl_write(conn, post_data, strlen(post_data));
+    http_ssl_read(conn);
 
     http_ssl_global_fini();
 
